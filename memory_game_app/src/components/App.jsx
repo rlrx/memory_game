4db60b5706b43da7cee9dd282b0257deba34676c
Ryan <ryan.lee.ruixiang@gmail.com>
2024-10-04 22:19:52 +0800

    Basic functionalities of game completed

--- memory_game_app/src/components/App.jsx
+++ memory_game_app/src/components/App.jsx
@@ -1,35 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Card from "./card.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+	let nameArray = [
+		"pikachu",
+		"bulbasaur",
+		"charmander",
+		"venusaur",
+		"charmeleon",
+		"charizard",
+		"squirtle",
+		"wartortle",
+		"blastoise",
+		"caterpie",
+		"metapod",
+		"ivysaur",
+	];
+	// .map to create new array with {name, clicks}
+	let initialArray = nameArray.map((pokemonName) => {
+		return { name: pokemonName, clicks: 0 };
+	});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// setStates for array, score and bestScore
+	let [pokeArray, setPokeArray] = useState(initialArray);
+	let [score, setScore] = useState(0);
+	let [highScore, setHighscore] = useState(0);
+
+	// reshuffleCards function (onClick function for each of the card buttons)
+	function reshuffleCards(pokemonName) {
+		let updatedCopyPokeArray = pokeArray.map((pokemon) => {
+			if (pokemon.name === pokemonName) {
+				return { ...pokemon, clicks: pokemon.clicks + 1 };
+			} else {
+				return pokemon;
+			}
+		});
+		// check if array has any pokemon with clicks more than 1
+		let greaterThanOneClicks = updatedCopyPokeArray.some(
+			(pokemon) => pokemon.clicks > 1
+		);
+		if (greaterThanOneClicks) {
+			setScore(0);
+			setHighscore((prevhighScore) => Math.max(prevhighScore, score));
+			setPokeArray(initialArray);
+		} else {
+			setScore(score + 1);
+			let shuffledArray = reshuffleArray(updatedCopyPokeArray);
+			setPokeArray(shuffledArray);
+		}
+	}
+
+	// reshuffleArray function
+	// return an array in new random order
+	// setState of the array to this new array
+	function reshuffleArray(updatedCopyArray) {
+		let numbers = [...updatedCopyArray];
+
+		//Shuffle the array using the Fisher-Yates (Knuth) shuffle algorithm
+		for (let i = numbers.length - 1; i > 0; i--) {
+			let j = Math.floor(Math.random() * (i + 1));
+			[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+		}
+
+		return numbers;
+	}
+
+	return (
+		<>
+			<header>
+				<div className="title-container">
+					<div className="title">Pokémon Memory Game</div>
+					<div className="instructions">
+						Click any of the cards below to start. Your goal is to
+						capture all 12 Pokémon once!
+					</div>
+				</div>
+				<div className="score-container">
+					<div className="score">Current Score: {score}</div>
+					<div className="best-score">Highest Score: {highScore}</div>
+				</div>
+			</header>
+			<div className="card-container">
+				<Card
+					pokemonName={pokeArray[0].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[1].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[2].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[3].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[4].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[5].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[6].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[7].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[8].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[9].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[10].name}
+					clickFunc={reshuffleCards}
+				/>
+				<Card
+					pokemonName={pokeArray[11].name}
+					clickFunc={reshuffleCards}
+				/>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- memory_game_app/src/components/card.jsx
+++ memory_game_app/src/components/card.jsx
@@ -1,34 +1,39 @@
 import { useEffect } from "react";
 
-function Card({ pokemonName }) {
-  useEffect(() => {
-    async function fetchPokemonData() {
-      try {
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`
-        ); // Replace with the API URL
-        if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
-        }
-        const data = await response.json(); // Parse the JSON from the response
-        console.log(data); // Use the fetched data
-        let imageUrl = data.sprites.front_default;
-        let cardNo = document.querySelector(
-          "[data-name=" + CSS.escape(pokemonName) + "]"
-        );
-        cardNo.src = imageUrl;
-      } catch (error) {
-        console.error("There was a problem with the fetch operation:", error);
-      }
-    }
-    fetchPokemonData(); // Call the async function
-  }, [pokemonName]);
-  return (
-    <button>
-      <img data-name={pokemonName}></img>
-      {pokemonName}
-    </button>
-  );
+function Card({ pokemonName, clickFunc }) {
+	useEffect(() => {
+		async function fetchPokemonData() {
+			try {
+				const response = await fetch(
+					`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`
+				); // Replace with the API URL
+				if (!response.ok) {
+					throw new Error(
+						"Network response was not ok " + response.statusText
+					);
+				}
+				const data = await response.json(); // Parse the JSON from the response
+				//console.log(data); // Use the fetched data
+				let imageUrl = data.sprites.front_default;
+				let cardNo = document.querySelector(
+					"[data-name=" + CSS.escape(pokemonName) + "]"
+				);
+				cardNo.src = imageUrl;
+			} catch (error) {
+				console.error(
+					"There was a problem with the fetch operation:",
+					error
+				);
+			}
+		}
+		fetchPokemonData(); // Call the async function
+	}, [pokemonName]);
+	return (
+		<button className="card" onClick={() => clickFunc(pokemonName)}>
+			<img data-name={pokemonName}></img>
+			{pokemonName}
+		</button>
+	);
 }
 
-export { Card };
+export default Card;
